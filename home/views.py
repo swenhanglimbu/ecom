fc7ae5a173917cd9ea46dcf048ded6804e21f13d
Swen Limbu <swenlimbu@Swens-MacBook-Air.local>
2022-12-25 18:08:05 +0545

    modified templates,and added some querry on views

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import View
+
 from .models import *
 # Create your views here.
-class Baseview(View):
+class BaseView(View):
     views = {}
 
-class Homeview(Baseview):
-    def get(self,request):
+class HomeView(BaseView):
+    def get(self, request):
+        self.views['categories'] = Category.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        self.views['new_products'] = Product.objects.filter(labels='new', stock='In stock')
+        self.views['hot_products'] = Product.objects.filter(labels='hot', stock='In stock')
+        self.views['sale_products'] = Product.objects.filter(labels='sale', stock='In stock')
 
-        return render(request, 'index.html')
+        return render(request, 'index.html', self.views)
